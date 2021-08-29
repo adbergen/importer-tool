add5ff23e928cc9c0834fe1a234a802e9c7d9428
Anthony Bergen <anthonydbergen@gmail.com>
2021-08-29 17:14:49 -0400

    added script for importer tool

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const mongodb = require("mongodb").MongoClient;
+const csvtojson = require("csvtojson");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// let url = "mongodb://username:password@localhost:27017/";
+let url = "mongodb://localhost:27017/";
 
-var app = express();
+csvtojson()
+  .fromFile("CityHealth Score Overview 2020_prelim.csv")
+  .then(csvData => {
+    console.log(csvData);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+    mongodb.connect(
+      url,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      (err, client) => {
+        if (err) throw err;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+        client
+          .db("zkoder_db")
+          .collection("category")
+          .insertMany(csvData, (err, res) => {
+            if (err) throw err;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+            console.log(`Inserted: ${res.insertedCount} rows`);
+            client.close();
+          });
+      }
+    );
+  });
